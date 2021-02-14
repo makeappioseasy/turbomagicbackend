806d13513a3f63fdd3cbff29ea4d0db73edbd887
เอกพงษ์ เทปิน <babydekdee@Mamoy-MacBook-Air.local>
2021-02-14 11:08:46 +0700

    change all table backend

---
+++ config/database.js
@@ -0,0 +1,28 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        /*
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'podtomaticdb'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'mysql1234'),
+        ssl: env.bool('DATABASE_SSL', false),
+        */
+        
+        host: env('DATABASE_HOST', 'dpg-c0k9o3bjbvm6a4mhaa0g'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'podtomatic'),
+        username: env('DATABASE_USERNAME', 'podtomatic_user'),
+        password: env('DATABASE_PASSWORD', 'HpvTAfXjgEyJooGyikmFCw04c6HHQqjn'),
+        ssl: env.bool('DATABASE_SSL', false)
+        
+      },
+      options: {}
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd9b3ffb1-390f-4aeb-bfd0-9fc04063dea7'
+};
