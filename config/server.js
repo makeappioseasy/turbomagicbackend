df7a741dce2bb2622003295725c700ba82cbb439
เอกพงษ์ เทปิน <babydekdee@Mamoy-MacBook-Air.local>
2021-02-24 15:54:10 +0700

    update genquote database

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  //host: env('HOST', '192.168.2.34'),
+  // host: env('HOST', '192.168.2.34'),
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'aaab3b9432fe1a2b49bd36e50e5752d19d6'),
+      secret: env('ADMIN_JWT_SECRET', 'podtomaticab3b9432fe1a2b49bd36e50e5752d19d6'),
     },
   },
 });
