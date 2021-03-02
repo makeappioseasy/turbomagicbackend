a8f8760bb7bb90b836d01839a5ea2b00e434d4a3
เอกพงษ์ เทปิน <babydekdee@Mamoy-MacBook-Air.local>
2021-03-02 18:04:02 +0700

    add boolean in genoccasions

--- config/database.js
+++ config/database.js
@@ -13,6 +13,7 @@ module.exports = ({ env }) => ({
         password: env('DATABASE_PASSWORD', 'mysql1234'),
         ssl: env.bool('DATABASE_SSL', false),
         */
+
         
         host: env('DATABASE_HOST', 'dpg-c0k9o3bjbvm6a4mhaa0g'),
         port: env.int('DATABASE_PORT', 5432),
@@ -21,6 +22,14 @@ module.exports = ({ env }) => ({
         password: env('DATABASE_PASSWORD', 'HpvTAfXjgEyJooGyikmFCw04c6HHQqjn'),
         ssl: env.bool('DATABASE_SSL', false)
         
+        /*
+       host: env('DATABASE_HOST', 'oregon-postgres.render.com'),
+       port: env.int('DATABASE_PORT', 5432),
+       database: env('DATABASE_NAME', 'podtomatic'),
+       username: env('DATABASE_USERNAME', 'podtomatic_user'),
+       password: env('DATABASE_PASSWORD', 'HpvTAfXjgEyJooGyikmFCw04c6HHQqjn'),
+       ssl: env.bool('DATABASE_SSL', true)
+        */
       },
       options: {}
     },
